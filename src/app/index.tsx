7662e3888fd42ecea430407772475c57c34f3c91
sparkie11 <gautamwise.s@gmail.com>
2024-11-05 15:57:14 +0530

    tailwind setup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,6 @@ import { Text } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <Text>index</Text>
+    <Text className="font-bold text-4xl bg-red-500 p-10 ">Index</Text>
   )
 }
