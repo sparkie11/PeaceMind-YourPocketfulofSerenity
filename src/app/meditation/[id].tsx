d609bf24a8d3a5ab1f72d96ef2302f603f2870f3
sparkie11 <gautamwise.s@gmail.com>
2024-11-06 12:48:35 +0530

    meditation details screen

--- src/app/meditation/[id].tsx
+++ src/app/meditation/[id].tsx
@@ -15,16 +15,18 @@ export default function MeditationDetails() {
         return <Text>Meditation not found</Text>  
     }
     return (
-        <SafeAreaView className="bg-orange-500 flex-1 p-2">
-            <View className="flex-row items-center  justify-between p-10">
-                <FontAwesome name="info" size={24} color="black" />
-                <View className="bg-zinc-900 p--2 rounded-md">
-                    <Text className="text-zinc-100 font-semibold "> Today's Meditation</Text>
-                </View>
+        <SafeAreaView className="bg-orange-500 flex-1 p-2 justify-between">
+            <View>
+                <View className="flex-row items-center  justify-between p-10">
+                    <FontAwesome name="info" size={24} color="black" />
+                    <View className="bg-zinc-900 p--2 rounded-md">
+                        <Text className="text-zinc-100 font-semibold "> Today's Meditation</Text>
+                    </View>
 
-                <FontAwesome onPress={()=>router.back()}  className="  right-4 z-10 " name="close" size={24} color="black" />
+                    <FontAwesome onPress={()=>router.back()}  className="  right-4 z-10 " name="close" size={24} color="black" />
+                </View>
+                <Text className="text-3xl mt-10 text-zinc-800 text-center font-semibold ">  {meditation?.title}</Text>
             </View>
-            <Text className="text-3xl mt-10 text-zinc-800 text-center font-semibold ">  {meditation?.title}</Text>
 
             <Pressable className="bg-zinc-800 self-center p-6 rounded-full w-20 aspect-square items-center  ">
                 
@@ -32,8 +34,14 @@ export default function MeditationDetails() {
                 {/* <FontAwesome6 name="pause" size={24} color="black" /> */}
             </Pressable>
 
-            <View>
-                
+            <View className="p-5 ">
+                <View className="flex-row justify-between">
+                    <FontAwesome name="eercast" size={24} color="black" />
+                    <FontAwesome name="cog" size={24} color="#3A3937" />
+                </View>
+                <View>
+                    <View className="bg-zinc-400 h-2"/>
+                </View>
             </View>
         
         </SafeAreaView>
